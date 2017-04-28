2763520063c6186de275c15efe134bffabbcb3c2
Luka Vandervelden <lukc@upyum.com>
2017-04-28 21:03:21 +0200

    Lang fixed.

--- generation.sh
+++ generation.sh
@@ -4,5 +4,6 @@ pandoc droit.md -o droit.pdf -N -S --toc \
 	--latex-engine=xelatex \
 	-V babel-otherlangs=french \
 	-V documentclass=article \
-	--template template.tex
+	--template template.tex \
+	-H header.tex
 
